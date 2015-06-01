0f6944e3aa703edefff9ad15d169baa069737908
SeihakRithy Muth <Rithy58@gmail.com>
2015-06-01 15:48:08 -0400

    restructured directory

--- firebase/app/app.js
+++ firebase/app/app.js
@@ -1,12 +1,5 @@
 var app = angular.module('angularChess', ["firebase"]);
 
-/*app.controller('Controller', ["$scope", "$firebaseObject"], function ($scope) {
-$scope.name = 'Guest';
-var ref = new Firebase("https://angular-chess.firebaseio.com/users/");
-var usersObj = $firebaseObject(ref);
-usersObj.$bindTo($scope, "userName");
-});*/
-
 app.controller("Controller", ["$scope", "$firebaseArray", "$firebaseAuth",
 function($scope, $firebaseArray, $firebaseAuth) {
 
